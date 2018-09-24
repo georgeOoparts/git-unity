ca78bffd21082b7d11b36e718b9f845d2aa04550
georgeOoparts <urataffy@gmail.com>
2018-09-24 19:52:07 +0900

    add marg1.cs      ::aaaa

---
+++ marg1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//aaaa
+public class marg1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
