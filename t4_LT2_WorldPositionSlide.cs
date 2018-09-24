ce87ab366178e9471d1b57fdcdf4f0b6b78b2853
georgeOoparts <urataffy@gmail.com>
2018-09-25 00:02:29 +0900

    add t4

---
+++ t4_LT2_WorldPositionSlide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class t4_LT2_WorldPositionSlide : MonoBehaviour {
+    //k4_a:どこかに書いてあるRectTransformの変数を作る
+    RectTransform rt;
+    //leftTopのテキストをインスペで当てはめる
+    public GameObject ltText;
+    //k2_a:どこかに書かれている。Textというクラスを扱うための変数を作成
+    Text ltTx;
+    // Use this for initialization
+    void Start () {
+        //k4_aa:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
+        rt = this.gameObject.GetComponent<RectTransform>();
+        //k2_aa:Textをこのオブジェクトで使うためのおまじない
+        ltTx = ltText.GetComponent<Text>();
+    }
+	// Update is called once per frame
+	void Update () {
+        //k4_aac1:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
+        rt.anchoredPosition = new Vector2(0,-ltTx.preferredHeight);
+    }
+}
