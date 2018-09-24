c68ebbddd697ebfee52d85244349937169a3063f
georgeOoparts <urataffy@gmail.com>
2018-09-24 11:07:40 +0900

    add t1_1_screenPointText.cs

---
+++ t1_1_screenPointText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//t1_1_screenPointText
+//text ui にアタッチする。マウスのスクリーンポイントをUIのtextに表示
+public class t1_1_screenPointText : MonoBehaviour {
+    Text text;
+    // Use this for initialization
+    void Start () {
+        text = this.gameObject.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "screen:: " + Input.mousePosition.ToString();
+    }
+}
