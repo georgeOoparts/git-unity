c432517c1302fa38031635a7ecca72c7424f32af
georgeOoparts <urataffy@gmail.com>
2018-09-24 11:20:46 +0900

    add  description with t1_1_screenPointText.cs

--- t1_1_screenPointText.cs
+++ t1_1_screenPointText.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 //t1_1_screenPointText
 //text ui にアタッチする。マウスのスクリーンポイントをUIのtextに表示
 public class t1_1_screenPointText : MonoBehaviour {
-    Text text;
+    Text text;//どこかに書かれている。Textというクラスを扱うための変数を作成
     // Use this for initialization
     void Start () {
+        //Textをこのオブジェクトで使うためのおまじない
         text = this.gameObject.GetComponent<Text>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //text.text = "・・・ "でTEXTのないよう変更。
+        //Input.mousePosition.ToString()でマウスのスクリーンポイント表示
         text.text = "screen:: " + Input.mousePosition.ToString();
     }
 }
