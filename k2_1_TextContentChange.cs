0ab8cee5c808f7f9f9b8cb04769ad8de40662c1e
georgeOoparts <urataffy@gmail.com>
2018-09-24 11:34:49 +0900

    add k2_1_TextContentChange.cs

---
+++ k2_1_TextContentChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
+
+//text ui にアタッチする。マウスのスクリーンポイントをUIのtextに表示
+public class k2_1_TextContentChange : MonoBehaviour {
+    //k2_1_a:どこかに書かれている。Textというクラスを扱うための変数を作成
+    Text text;
+
+    void Start () {
+        //k2_1_b:Textをこのオブジェクトで使うためのおまじない
+        text = this.gameObject.GetComponent<Text>();
+        //k2_1_c:text.text = "・・・ "でTEXTのないよう変更。
+        text.text = "screen:: ";
+    }
+}
