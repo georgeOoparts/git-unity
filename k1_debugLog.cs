701b4321b9961abeda6425777af9c99eeac84f30
georgeOoparts <urataffy@gmail.com>
2018-09-24 09:00:06 +0900

    Ｋ１　デバックログのやり方

--- k1_debugLog.cs
+++ k1_debugLog.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class k1_debugLog : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("デバックログはこうやるぜ");
 	}
 	
 	// Update is called once per frame
