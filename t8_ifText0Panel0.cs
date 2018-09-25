4035a8eab6a2d7fea83c905668273bc63bd99d3a
georgeOoparts <urataffy@gmail.com>
2018-09-25 19:57:58 +0900

    add t8

---
+++ t8_ifText0Panel0.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class t8_ifText0Panel0 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("t8_ifText0Panel0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
