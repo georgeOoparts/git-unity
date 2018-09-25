922b99fae5ffe2a2ab4c247f464e37304a74062c
georgeOoparts <urataffy@gmail.com>
2018-09-25 16:48:37 +0900

    add t6

---
+++ t6_contentNarabe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t6_contentNarabe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("t6_contentNarabe");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
