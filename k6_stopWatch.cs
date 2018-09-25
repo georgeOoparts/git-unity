db3b0a40570258ec27cb69db4e992a0b0d3d7c53
georgeOoparts <urataffy@gmail.com>
2018-09-25 16:17:19 +0900

    kaizen k6

--- k6_stopWatch.cs
+++ k6_stopWatch.cs
@@ -2,34 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//k6_stopwatch:
 public class k6_stopWatch : MonoBehaviour {
+    //k6_a:ストップウォッチ関数を使う時のおまじない。
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
+    // 何秒たったかを変数elapseに入れる
     private float elapse;
-    private bool mojiOnOff = true;
     public float interval = 0.35f;
     // Use this for initialization
     void Start () {
+        //k6_aa:ストップウォッチスタート
         stopwatch.Start();
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        //k6_ab:ストップウォッチの時間をリセット
+        stopwatch.Reset();
+        //k6_ac:何秒たったかを変数elapseに入れる
         elapse = (float)stopwatch.Elapsed.TotalSeconds;
         //Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
-        if (mojiOnOff == true) {
-            if (elapse >= interval) {
-                mojiOnOff = false;
-                this.gameObject.GetComponent<Image>().enabled = false;
-                stopwatch.Reset();
-                stopwatch.Start();
-            }
-        } else if (elapse >= interval) {
-            mojiOnOff = true;
-            this.gameObject.GetComponent<Image>().enabled = true;
-            stopwatch.Reset();
-            stopwatch.Start();
-        }
     }
 }
