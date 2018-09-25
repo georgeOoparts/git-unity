01a220d1408bfa3a6c2924fb8b4450eb2d4af72a
georgeOoparts <urataffy@gmail.com>
2018-09-25 16:37:23 +0900

    kaizen t5 k6

--- k6_stopWatch.cs
+++ k6_stopWatch.cs
@@ -8,7 +8,7 @@ public class k6_stopWatch : MonoBehaviour {
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
     // 何秒たったかを変数elapseに入れる
-    private float elapse;
+    //private float elapse;
     //public float interval = 0.35f;
     // Use this for initialization
     void Start () {
@@ -17,7 +17,7 @@ public class k6_stopWatch : MonoBehaviour {
         //k6_ab:ストップウォッチの時間をリセット
         stopwatch.Reset();
         //k6_ac:何秒たったかを変数elapseに入れる
-        elapse = (float)stopwatch.Elapsed.TotalSeconds;
+        //elapse = (float)stopwatch.Elapsed.TotalSeconds;
         //Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
     }
 }
