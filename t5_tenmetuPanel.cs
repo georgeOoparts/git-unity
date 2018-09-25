2690e676c3b62160590f658955d23329a5f4a29c
georgeOoparts <urataffy@gmail.com>
2018-09-25 15:55:17 +0900

    kaizen t5

--- t5_tenmetuPanel.cs
+++ t5_tenmetuPanel.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class t5_tenmetuPanel : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private System.Diagnostics.Stopwatch stopwatch
+        = new System.Diagnostics.Stopwatch();
+    private float elapse;
+    private bool mojiOnOff = true;
+    public float interval = 0.35f;
+    // Use this for initialization
+    void Start () {
+        stopwatch.Start();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        elapse = (float)stopwatch.Elapsed.TotalSeconds;
+        //Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
+        if (mojiOnOff == true) {
+            if (elapse >= interval) {
+                mojiOnOff = false;
+                this.gameObject.GetComponent<Image>().enabled = false;
+                stopwatch.Reset();
+                stopwatch.Start();
+            }
+        } else if (elapse >= interval) {
+            mojiOnOff = true;
+            this.gameObject.GetComponent<Image>().enabled = true;
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+    }
 }
