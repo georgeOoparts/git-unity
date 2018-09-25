49ef84715946c519dd6d1be4a46943de1ee39210
georgeOoparts <urataffy@gmail.com>
2018-09-25 21:56:13 +0900

    kaizen t8

--- t8_panelLine.cs
+++ t8_panelLine.cs
@@ -46,19 +46,25 @@ public class t8_panelLine : MonoBehaviour {
         //k4_aac1:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
         rt1.anchoredPosition = new Vector2(0, 0);
         plusScreenWidth =rt1.sizeDelta.x;
+
         rt2.anchoredPosition = new Vector2(plusScreenWidth, 0);
         plusScreenWidth = rt1.sizeDelta.x+ rt2.sizeDelta.x;
+
         rt3.anchoredPosition = new Vector2(plusScreenWidth, 0);
         plusScreenWidth = rt1.sizeDelta.x + rt2.sizeDelta.x+ rt3.sizeDelta.x;
+
         rt4.anchoredPosition = new Vector2(plusScreenWidth, 0);
         plusScreenWidth = rt1.sizeDelta.x + rt2.sizeDelta.x + rt3.sizeDelta.x
             + rt4.sizeDelta.x;
+
         rt5.anchoredPosition = new Vector2(plusScreenWidth, 0);
         plusScreenWidth = rt1.sizeDelta.x + rt2.sizeDelta.x + rt3.sizeDelta.x
             + rt4.sizeDelta.x+ rt5.sizeDelta.x;
+
         rt6.anchoredPosition = new Vector2(plusScreenWidth, 0);
         plusScreenWidth = rt1.sizeDelta.x + rt2.sizeDelta.x + rt3.sizeDelta.x
             + rt4.sizeDelta.x + rt5.sizeDelta.x+ rt6.sizeDelta.x;
+
         rt7.anchoredPosition = new Vector2(plusScreenWidth, 0);
     }
 }
