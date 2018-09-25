34641578b9edb2588ea1e330d33e3ce5c53542e8
georgeOoparts <urataffy@gmail.com>
2018-09-25 19:36:18 +0900

    add k9

---
+++ k9_mousebutton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
+public class k9_mousebutton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("k9_mousebutton");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
