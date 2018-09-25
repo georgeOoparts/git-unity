321bcdeb97cff249c0e82fc63f53751a522fa3c3
georgeOoparts <urataffy@gmail.com>
2018-09-25 09:25:04 +0900

    add k5 about static

---
+++ k5_atStatic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class k5_atStatic : MonoBehaviour {
+    //k5_a_atStatic:静的変数を外のクラスから（クラス名）.(メソッド名)で呼び出す。
+    public static int c = 200;
+    //k5_b_atStatic:静的メソッドを外のクラスから（クラス名）.(メソッド名)で呼び出す。
+    public static int plus(int A, int B) {
+        return (A * B);
+    }
+}
