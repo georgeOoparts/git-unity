a277cc04fb2d1ac7324ff3f479e40d79d5d4e086
georgeOoparts <urataffy@gmail.com>
2018-09-25 18:48:15 +0900

    add k8

---
+++ k8_oyaKodomoObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
+public class k8_oyaKodomoObj : MonoBehaviour {
+    GameObject kodomoObj1;
+    GameObject kodomoObj2;
+    // Use this for initialization
+    void Start () {
+        //k8_a:子(孫、ひ孫　無限にＯＫ)になっているＯＢＪを得る。
+        kodomoObj1 = this.gameObject.transform.GetChild(0).gameObject;
+        //k8_a:応用。孫になっているＯＢＪを得る。
+        kodomoObj2 = this.gameObject.transform.GetChild(1).gameObject;
+    }
+}
--- t6_panelSizeSet.cs
+++ t6_panelSizeSet.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
+
 //テキストを子にもつ、textPanelのサイズを　子のＴＥＸＴにばっちり合わせる。
 //このオブジェのアンカーをストレッチにするとうまく動かない。
 public class t6_panelSizeSet : MonoBehaviour {
-    Text kodomoText;
-    RectTransform Rt;
     GameObject kodomoObj;
+    //k2_a:どこかに書かれている。Textというクラスを扱うための変数を作成
+    Text kodomoText;
+    //k4_a:どこかに書いてあるRectTransformの変数を作る
+    RectTransform Rt;
     public float yohaku = 10;
     // Use this for initialization
     void Start() {
+        //
         kodomoObj = this.gameObject.transform.GetChild(0).gameObject;
         kodomoText = kodomoObj.GetComponent<Text>();
         Rt = this.gameObject.GetComponent<RectTransform>();
