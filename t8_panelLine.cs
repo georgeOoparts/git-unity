0306db9709f693dbf5c40d6fcca9989af01e9ae5
georgeOoparts <urataffy@gmail.com>
2018-09-25 20:36:19 +0900

    add t8

---
+++ t8_panelLine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t8_panelLine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("t8_panelLine");
+	}
+}
