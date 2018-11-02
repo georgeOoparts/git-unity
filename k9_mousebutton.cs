6f944b9dd3be0092886b92f675570b2aef360911
georgeOoparts <urataffy@gmail.com>
2018-11-02 13:35:42 +0900

    add  t14

--- k9_mousebutton.cs
+++ k9_mousebutton.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 public class k9_mousebutton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        //k9_a:左クリック入力　Input.GetMouseButtonDown(0)
-        //具体例：：if (Input.GetMouseButtonDown(0)) count++;
-    }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    //k9_a:左クリック入力 ボタンダウンInput.GetMouseButtonDown(0)
+    //具体例：：if (Input.GetMouseButtonDown(0)) count++;
+
+    //k9_b:左クリック入力　ボタンアップInput.GetMouseButtonUp(0)
+    //具体例：：if (Input.GetMouseButtonDown(0)) count++;
+   
 }
---
+++ t14_rulerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UI使うときはこれなきゃ駄目
+
+public class t14_rulerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("rulerMove");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
