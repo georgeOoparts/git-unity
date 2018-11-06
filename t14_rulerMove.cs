daa3bd3f7ceeb19708379db93b4b587b562671f0
georgeOoparts <urataffy@gmail.com>
2018-11-06 20:32:00 +0900

    kaizen t14

--- t14_rulerMove.cs
+++ t14_rulerMove.cs
@@ -11,23 +11,35 @@ public class t14_rulerMove : MonoBehaviour {
         //Debug.Log("rulerMove");
 	}
 	void Update () {
-        playerControl();
+        //下に書いてあるメソッドを呼び出す
+        flickControl();
     }
-    private void playerControl() {
-
+    private void flickControl() {
+        //k3_a:Input.mousePosition.ToString()でマウスのスクリーンポイント表示
+        //k3_zz2_a:スクリーン座標＞ワールド座標
+        //マウスを押したら
         if (Input.GetMouseButtonDown(0)) {
             objectPos = this.transform.position;
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+        //マウスを押してる最中
         if (Input.GetMouseButton(0)) {
 
-            Vector3 prePos = this.transform.position;
-            Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
+            //Vector3 prePos = this.transform.position;
+            Vector3 diff = 
+                Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
 
+            //タッチ対応デバイス向け、1本目の指にのみ反応
+            //if (Input.touchSupported) {
+            //    diff =
+            //Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position) 
+            //- mousePos;
+            //}
             diff.z = 0.0f;
             this.transform.position = objectPos + diff;
 
         }
+        //マウスを上げたら
         if (Input.GetMouseButtonUp(0)) {
             objectPos = Vector3.zero;
             mousePos = Vector3.zero;
