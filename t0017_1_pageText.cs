5b85daeb3edbc8713db25fa68e16c8b26fcfabfd
georgeOoparts <urataffy@gmail.com>
2018-11-22 16:22:26 +0900

    t17   text position move

--- t0017_1_pageText.cs
+++ t0017_1_pageText.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 
 public class t0017_1_pageText : MonoBehaviour {
-
-	// Use this for initialization
+       
+    Transform page;
 	void Start () {
-        Debug.Log("pagetext");
-	}
+        page = this.gameObject.GetComponent<Transform>();
+        
+        //Debug.Log("pagetext");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //for (int i = 1; i == 9; i++) {
+            //transform.position = new Vector3(0, 5 - 10 * (i - 1), 0);
+        //}
+        transform.position = new Vector3((float)-2.8, -5, 0);
+    }
 }
