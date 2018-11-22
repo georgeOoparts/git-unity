87ef485d3bef9879995cd97ed01afbb1fed58501
georgeOoparts <urataffy@gmail.com>
2018-11-23 00:11:45 +0900

    t16 kaizen

--- t0016_1_swipe.cs
+++ t0016_1_swipe.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 public class t0016_1_swipe : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
-        //Debug.Log("swipe");
+        Debug.Log("swipe");
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
