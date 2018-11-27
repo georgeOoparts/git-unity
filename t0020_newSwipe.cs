69028c2d80b04bfdf6ea015ca4929a77353c63db
georgeOoparts <urataffy@gmail.com>
2018-11-27 21:51:17 +0900

    t20 kaizen

--- t0020_newSwipe.cs
+++ t0020_newSwipe.cs
@@ -6,11 +6,45 @@ public class t0020_newSwipe : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("newSwipe");
+        //Debug.Log("newSwipe");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void flickControl() {
+        //フリックをするメソッド
+        //k3_a:Input.mousePosition.ToString()でマウスのスクリーンポイント表示
+        //k3_zz2_a:スクリーン座標＞ワールド座標
+        //マウスを押したら
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 objectPos = this.transform.position;
+            Vector3  mousePos = 
+                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        //マウスを押してる最中
+        if (Input.GetMouseButton(0)) {
+
+            //Vector3 prePos = this.transform.position;
+            Vector3 diff =
+                Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
+
+            //タッチ対応デバイス向け、1本目の指にのみ反応
+            //if (Input.touchSupported) {
+            //    diff =
+            //Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position) 
+            //- mousePos;
+            //}
+            diff.z = 0.0f;
+            this.transform.position = objectPos + diff;
+
+        }
+        //マウスを上げたら
+        if (Input.GetMouseButtonUp(0)) {
+            objectPos = Vector3.zero;
+            mousePos = Vector3.zero;
+        }
+    }
 }
