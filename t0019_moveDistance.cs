de6e758ba82c439c64ae3826ce7746403d559ef5
georgeOoparts <urataffy@gmail.com>
2018-11-28 00:16:46 +0900

    kaizen t19

--- t0019_moveDistance.cs
+++ t0019_moveDistance.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0019_moveDistance : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    Vector3 objectPos;
+    Vector3 firstPos;
+    void Start () {
         Debug.Log("move");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0)) {
+            //
+            firstPos =
+                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        //マウスを押してる最中
+        if (Input.GetMouseButton(0)) {
+           
+            //フリックの感覚にする。下にフリックすると上へ移動
+            Vector3 diff =
+                firstPos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Debug.Log("f::" + firstPos
+               // +"now::"+ Camera.main.ScreenToWorldPoint(Input.mousePosition)
+                + "diff::"+ diff.y);
+
+        }
+    }
 }
