66845ab7985bd41b943efa63e8d0f4c05e8d1341
georgeOoparts <urataffy@gmail.com>
2018-11-28 12:28:37 +0900

    t19 とりあえず　完成

--- t0019_moveDistance.cs
+++ t0019_moveDistance.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 //クリックした所からスワイプ先の距離を表示
-//t20newSwipeと一緒に使うとうまくいかない
+//とりあえずスクリーンポイント
 
 public class t0019_moveDistance : MonoBehaviour {
     Vector3 objectPos;
@@ -15,15 +15,15 @@ public class t0019_moveDistance : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
             //
-            firstPos =
-                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            firstPos = Input.mousePosition;
+                //Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         //マウスを押してる最中
         if (Input.GetMouseButton(0)) {
-           
+
             //フリックの感覚にする。下にフリックすると上へ移動
-            Vector3 diff =
-                firstPos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 diff = firstPos - Input.mousePosition;
+            //Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Debug.Log("f::" + firstPos
                 //+"now::"+ Camera.main.ScreenToWorldPoint(Input.mousePosition)
