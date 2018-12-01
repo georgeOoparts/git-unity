2794387893bf85c81034b7b0c1a0875fbb67ac87
georgeOoparts <urataffy@gmail.com>
2018-12-02 00:18:57 +0900

    add t21

---
+++ t0021_kaokudoSwipe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t0021_kaokudoSwipe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("dddddddddddd");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
