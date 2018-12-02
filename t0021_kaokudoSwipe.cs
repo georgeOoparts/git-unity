3d0fe48a2eb594d3133f08db7a92b23b2a5110dc
georgeOoparts <urataffy@gmail.com>
2018-12-02 11:46:02 +0900

    t21 kaizen

--- t0021_kaokudoSwipe.cs
+++ t0021_kaokudoSwipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0021_kaokudoSwipe : MonoBehaviour {
-    public float kasokudo = 5;
+    public float kasokudo = 10;
 	// Use this for initialization
 	void Start () {
         
