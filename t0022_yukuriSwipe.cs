d5855b4d3da87d409c01b57b2d715067dd403198
georgeOoparts <urataffy@gmail.com>
2018-12-04 15:51:40 +0900

    t22 add hensu

--- t0022_yukuriSwipe.cs
+++ t0022_yukuriSwipe.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0022_yukuriSwipe : MonoBehaviour {
-
+    public float bunkatuSuu = 10;
+    public float idouKyori = -10;
+    public float idouJikan = 2;
+    //public float bamenNagasa=
 	// Use this for initialization
 	void Start () {
         Debug.Log("kawaii");
