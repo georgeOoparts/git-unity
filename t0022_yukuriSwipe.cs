1cf031df3e3fd6537c7003f6ed5f8302880d756f
georgeOoparts <urataffy@gmail.com>
2018-12-04 16:11:29 +0900

    t22 とりあえずクリックして１０下に行く

--- t0022_yukuriSwipe.cs
+++ t0022_yukuriSwipe.cs
@@ -6,14 +6,21 @@ public class t0022_yukuriSwipe : MonoBehaviour {
     public float bunkatuSuu = 10;
     public float idouKyori = -10;
     public float idouJikan = 2;
-    //public float bamenNagasa=
-	// Use this for initialization
-	void Start () {
-        Debug.Log("kawaii");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //k6_a:ストップウォッチ関数を使う時のおまじない。
+    private System.Diagnostics.Stopwatch stopwatch
+        = new System.Diagnostics.Stopwatch();
+    // 何秒たったかを変数elapseに入れる
+    private float elapse;
+
+    void Update() {
+        //k6_ac:何秒たったかを変数elapseに入れる
+        elapse = (float)stopwatch.Elapsed.TotalSeconds;
+        //マウスを押した時
+        if (Input.GetMouseButtonDown(0)) {
+            //k6_aa:ストップウォッチスタート
+            stopwatch.Start();
+            this.transform.position +=new Vector3(0, idouKyori,0);
+        }
+    }
 }
