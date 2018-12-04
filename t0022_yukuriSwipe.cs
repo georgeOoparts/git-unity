b8099ab3337cd608870f148bbfcfc6113170ae5d
georgeOoparts <urataffy@gmail.com>
2018-12-04 16:54:35 +0900

    ｔ２２　timedeltaTime

--- t0022_yukuriSwipe.cs
+++ t0022_yukuriSwipe.cs
@@ -22,9 +22,12 @@ public class t0022_yukuriSwipe : MonoBehaviour {
             float perIdouJikan = idouJikan/bunkatuSuu;
             //k6_aa:ストップウォッチスタート
             stopwatch.Start();
-            for (float i=1;i<=bunkatuSuu;i++) {
-                this.transform.position += new Vector3(0, perIdoukyori, 0);
-            }
+            //for (float i=1;i<=bunkatuSuu;i++) {
+                this.transform.position +=
+                new Vector3(0, idouKyori*Time.deltaTime, 0);
+            //}
         }
+        this.transform.position +=
+               new Vector3(0, idouKyori * Time.deltaTime, 0);
     }
 }
