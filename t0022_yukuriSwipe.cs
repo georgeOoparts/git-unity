3bca6f03af498934b5a62a8fa65195212920209d
georgeOoparts <urataffy@gmail.com>
2018-12-04 15:43:43 +0900

    add t22

---
+++ t0022_yukuriSwipe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t0022_yukuriSwipe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("kawaii");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
