1c8b4c13c1464b496ecd07db9efa868c19274960
georgeOoparts <urataffy@gmail.com>
2018-12-05 03:19:31 +0900

    ｔ22　時間内の処理ＯＫ

--- t0022_yukuriSwipe.cs
+++ t0022_yukuriSwipe.cs
@@ -7,15 +7,28 @@ public class t0022_yukuriSwipe : MonoBehaviour {
     public float idouKyori = -10;
     public float idouJikan = 1;
 
+    public float timeOut=3;
+    private float timeElapsed;
+
+
     //k6_a:ストップウォッチ関数を使う時のおまじない。
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
     // 何秒たったかを変数elapseに入れる
     private float elapse;
-
+    int i=0;
     void Update() {
         //k6_ac:何秒たったかを変数elapseに入れる
         elapse = (float)stopwatch.Elapsed.TotalSeconds;
+        stopwatch.Start();
+
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut) {
+            // Do anything
+            Debug.Log(elapse);
+            timeElapsed = 0.0f;
+        }
         //マウスを押した時
         //if (Input.GetMouseButtonDown(0)) {
         //    float perIdoukyori = idouKyori / bunkatuSuu;
@@ -27,9 +40,23 @@ public class t0022_yukuriSwipe : MonoBehaviour {
         //        new Vector3(0, idouKyori*Time.deltaTime, 0);
         //    //}
         //}
-        if (Input.GetMouseButton(0)) {
-            this.transform.position +=
-               new Vector3(0, idouKyori * Time.deltaTime, 0);
-        }
+
+        //if (Time.deltaTime * i <= 5) {
+        //    stopwatch.Start();
+        //    this.transform.position +=
+        //       new Vector3(0, idouKyori * Time.deltaTime, 0);
+        //    i++; Debug.Log(elapse);
+        //}  
+        //if (Input.GetMouseButton(0)) {
+        //    Vector3 startPosition = new Vector3(0,0,0);
+        //    Vector3 endPosition = new Vector3(0, -10, 0);
+        //    float rate = 1;
+        //    stopwatch.Start();
+        //    this.transform.position +=
+        //       new Vector3(0, idouKyori * Time.deltaTime, 0);
+        //    //this.transform.position = 
+        //        //Vector3.Lerp(startPosition, endPosition, rate);
+        //}
+
     }
 }
