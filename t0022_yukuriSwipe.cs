30abd775d7bafd06ce1d116c0d12ec5e3cc778df
georgeOoparts <urataffy@gmail.com>
2018-12-05 02:00:38 +0900

    ｔ22　まうす押したら　移動

--- t0022_yukuriSwipe.cs
+++ t0022_yukuriSwipe.cs
@@ -17,17 +17,19 @@ public class t0022_yukuriSwipe : MonoBehaviour {
         //k6_ac:何秒たったかを変数elapseに入れる
         elapse = (float)stopwatch.Elapsed.TotalSeconds;
         //マウスを押した時
-        if (Input.GetMouseButtonDown(0)) {
-            float perIdoukyori = idouKyori / bunkatuSuu;
-            float perIdouJikan = idouJikan/bunkatuSuu;
-            //k6_aa:ストップウォッチスタート
-            stopwatch.Start();
-            //for (float i=1;i<=bunkatuSuu;i++) {
-                this.transform.position +=
-                new Vector3(0, idouKyori*Time.deltaTime, 0);
-            //}
-        }
-        this.transform.position +=
+        //if (Input.GetMouseButtonDown(0)) {
+        //    float perIdoukyori = idouKyori / bunkatuSuu;
+        //    float perIdouJikan = idouJikan/bunkatuSuu;
+        //    //k6_aa:ストップウォッチスタート
+        //    stopwatch.Start();
+        //    //for (float i=1;i<=bunkatuSuu;i++) {
+        //        this.transform.position +=
+        //        new Vector3(0, idouKyori*Time.deltaTime, 0);
+        //    //}
+        //}
+        if (Input.GetMouseButton(0)) {
+            this.transform.position +=
                new Vector3(0, idouKyori * Time.deltaTime, 0);
+        }
     }
 }
