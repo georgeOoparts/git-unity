c87e9eaad489ccd9bb252fd0739c58e4d0db0f89
georgeOoparts <urataffy@gmail.com>
2018-12-07 00:00:28 +0900

    kaizen t23 クリックして移動ＯＫ

--- t0023_1byouGo.cs
+++ t0023_1byouGo.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0023_1byouGo : MonoBehaviour {
-
-	// Use this for initialization
+    private bool go = false;
+    public float timeOut=1;
+    private float timeElapsed;
+    public float v=10;
 	void Start () {
-        Debug.Log("111111111111");		
-	}
+
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0))
+            go = true;
+        if (go) {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed <= timeOut) {
+                this.gameObject.transform.position +=
+                    new Vector3(0, -v * Time.deltaTime, 0);
+            } else {
+                go = false;
+                timeElapsed = 0;
+            }
+        }
 	}
 }
