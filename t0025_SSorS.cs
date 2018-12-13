a4e4eb53aa9affa2e4bc651b9f81f0759a291c49
georgeOoparts <urataffy@gmail.com>
2018-12-13 23:28:09 +0900

    add t25 SSとＳの切り分けプログラムでバックログ表示

---
+++ t0025_SSorS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t0025_SSorS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("SS or S");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
