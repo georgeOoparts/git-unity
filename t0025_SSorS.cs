3f41c0c392cb344c94ac927476c0afc18f447c3f
georgeOoparts <urataffy@gmail.com>
2018-12-13 23:48:02 +0900

    ｔ２５　まずストップウォッチを作ります。

--- t0025_SSorS.cs
+++ t0025_SSorS.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class t0025_SSorS : MonoBehaviour {
+    //k6_a:ストップウォッチ関数を使う時のおまじない。
+    private System.Diagnostics.Stopwatch stopwatch
+        = new System.Diagnostics.Stopwatch();
+    // 何秒たったかを変数elapseに入れる
+    private float elapse;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("SS or S");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //k6_ac:何秒たったかを変数elapseに入れる
+        elapse = (float)stopwatch.Elapsed.TotalSeconds;
+        //k6_aa:ストップウォッチスタート
+        stopwatch.Start();
+        ////マウスを押した時
+        //if (Input.GetMouseButtonDown(0)) {
+        //    //k6_aa:ストップウォッチスタート
+        //    stopwatch.Start();
+        //}
+        //if (Input.GetMouseButton(0)) {
+        //    //k6_ab:ストップウォッチの時間をリセット
+        //    stopwatch.Reset();
+        //    Debug.Log(elapse);
+        //}
+
+        Debug.Log(elapse);
+    }
 }
