720cf7f1e20cf93ebd66167c98878c30c4f52628
georgeOoparts <urataffy@gmail.com>
2018-12-14 19:01:34 +0900

    t23z1　とりあえずメソッド化成功

--- t0023_z1_1byougoKai.cs
+++ t0023_z1_1byougoKai.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0023_z1_1byougoKai : MonoBehaviour {
+    private bool go = false;
+    public float timeOut = 1;
+    private float timeElapsed;
+    public float v = 10;
+    public float t = 0.3f;
+    public float chousei = 10;
+    private float l = 1;
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("kai");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+            go = true;
+        if (go) {
+            sSwipe();
+        }
+    }
+    private void sSwipe() {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed <= timeOut) {
+            this.gameObject.transform.position +=
+                new Vector3(0, -chousei * l / t * Time.deltaTime, 0);
+        } else {
+            go = false;
+            timeElapsed = 0;
+        }
+    }
 }
