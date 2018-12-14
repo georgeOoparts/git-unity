365fb3559e84218d9b19e74beef6538424893a10
georgeOoparts <urataffy@gmail.com>
2018-12-14 18:54:11 +0900

    add t23z1 1秒後をメソッドかする。

---
+++ t0023_z1_1byougoKai.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t0023_z1_1byougoKai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("kai");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
