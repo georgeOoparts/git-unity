11cfad0bd56a43101bb07a606c4d6aa62737e546
georgeOoparts <urataffy@gmail.com>
2018-12-18 05:32:15 +0900

    t31z1   戻りポイント　とりあえずストップウォッチ

--- t0031_z1_sim1byouMethod.cs
+++ t0031_z1_sim1byouMethod.cs
@@ -7,40 +7,18 @@ public class t0031_z1_sim1byouMethod : MonoBehaviour {
     //k6_a:ストップウォッチ関数を使う時のおまじない。
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
+    // 何秒たったかを変数elapseに入れる
     private float elapse;
-    private bool mojiOnOff = true;
     public float interval = 0.35f;
 
-    // Use this for initialization
-    void Start() {
-        
+    
+    void Update() {
         //k6_aa:ストップウォッチスタート
         stopwatch.Start();
-    }
-
-    // Update is called once per frame
-    void Update() {
+        //k6_ab:ストップウォッチの時間をリセット
+        //stopwatch.Reset();
         //k6_ac:何秒たったかを変数elapseに入れる
         elapse = (float)stopwatch.Elapsed.TotalSeconds;
-        //Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
-        if (mojiOnOff == true) {
-            if (elapse >= interval) {
-                mojiOnOff = false;
-                //k7_a:オブジェを存在するけど見えなくする。
-                this.gameObject.GetComponent<Image>().enabled = false;
-                //k6_ab:ストップウォッチの時間をリセット
-                stopwatch.Reset();
-                //k6_aa:ストップウォッチスタート
-                stopwatch.Start();
-            }
-        } else if (elapse >= interval) {
-            mojiOnOff = true;
-            //k7_b:オブジェを見えるようにするよ。
-            this.gameObject.GetComponent<Image>().enabled = true;
-            //k6_ab:ストップウォッチの時間をリセット
-            stopwatch.Reset();
-            //k6_aa:ストップウォッチスタート
-            stopwatch.Start();
-        }
+        Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
     }
 }
