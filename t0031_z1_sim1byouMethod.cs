5a66021531297f3985b50d66b3aa50f35b23ed5a
georgeOoparts <urataffy@gmail.com>
2018-12-20 00:32:49 +0900

    ｔ３５　もともとあったelapse使うだけ　完成

--- t0031_z1_sim1byouMethod.cs
+++ t0031_z1_sim1byouMethod.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class t0031_z1_sim1byouMethod : MonoBehaviour {
-    //k6_a:ストップウォッチ関数を使う時のおまじない。
-    private System.Diagnostics.Stopwatch stopwatch
-        = new System.Diagnostics.Stopwatch();
-    // 何秒たったかを変数elapseに入れる
-    private float elapse;
-    public float interval = 3.00f;
+    ////k6_a:ストップウォッチ関数を使う時のおまじない。
+    //private System.Diagnostics.Stopwatch stopwatch
+    //    = new System.Diagnostics.Stopwatch();
+    //// 何秒たったかを変数elapseに入れる
+    //private float elapse;
+    //public float interval = 3.00f;
 
-    void Update() {
-        if (Input.GetMouseButton(0)) {
-            //timeHantei();
-            //Debug.Log();
+    //void Update() {
+    //    if (Input.GetMouseButton(0)) {
+    //        //timeHantei();
+    //        //Debug.Log();
             
-        }
-    }
-    int i = 0;
-    int timeHanteiHensu = 0;
-    void timeHantei() {
-        //k6_aa:ストップウォッチスタート
-        stopwatch.Start();
+    //    }
+    //}
+    //int i = 0;
+    //int timeHanteiHensu = 0;
+    //void timeHantei() {
+    //    //k6_aa:ストップウォッチスタート
+    //    stopwatch.Start();
         
-        while (true) {
-           //k6_ac:何秒たったかを変数elapseに入れる
-            elapse = (float)stopwatch.Elapsed.TotalSeconds;
+    //    while (true) {
+    //       //k6_ac:何秒たったかを変数elapseに入れる
+    //        elapse = (float)stopwatch.Elapsed.TotalSeconds;
 
-            if (elapse <= interval) {
-                //何秒たったかを表示させたいときはこれを使う
-                timeHanteiHensu =1;
-                //Debug.Log(elapse);
-                //return (1);
-            } else {
-                //k6_ab:ストップウォッチの時間をリセット
-                stopwatch.Reset();
-                //i++;
-                //Debug.Log(i+"回目");
-                //return (0);
-                timeHanteiHensu = 0;
-                break;
-            }
-        }
-    }
+    //        if (elapse <= interval) {
+    //            //何秒たったかを表示させたいときはこれを使う
+    //            timeHanteiHensu =1;
+    //            //Debug.Log(elapse);
+    //            //return (1);
+    //        } else {
+    //            //k6_ab:ストップウォッチの時間をリセット
+    //            stopwatch.Reset();
+    //            //i++;
+    //            //Debug.Log(i+"回目");
+    //            //return (0);
+    //            timeHanteiHensu = 0;
+    //            break;
+    //        }
+    //    }
+    //}
 }
--- t0035_simTupJikanBack.cs
+++ t0035_simTupJikanBack.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0035_simTupJikanBack : MonoBehaviour {
+    //k6_a:ストップウォッチ関数を使う時のおまじない。
+    private System.Diagnostics.Stopwatch stopwatch
+        = new System.Diagnostics.Stopwatch();
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // 何秒たったかを変数elapseに入れる
+    private float elapse;
+
+    void Update() {
+        //k6_ac:何秒たったかを変数elapseに入れる
+        elapse = (float)stopwatch.Elapsed.TotalSeconds;
+
+        ////マウスを押した時
+        if (Input.GetMouseButtonDown(0)) {
+            //k6_aa:ストップウォッチスタート
+            stopwatch.Start();
+        }
+        //マウスを押している最中
+        if (Input.GetMouseButton(0)) {
+            Debug.Log(elapse);
+        }
+        //マウスボタンを離したら
+        if (Input.GetMouseButtonUp(0)) {
+            Debug.Log(elapse);
+            //k6_ab:ストップウォッチの時間をリセット
+            stopwatch.Reset();
+        }
+    }
 }
