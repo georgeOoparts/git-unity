899ade3d3c27aaaf94ff286d4df317cf7363ae4c
georgeOoparts <urataffy@gmail.com>
2018-12-27 07:25:06 +0900

    t43　たてよこ判定完成

--- t0043_ym_tateyokoMethod.cs
+++ t0043_ym_tateyokoMethod.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class t0043_ym_tateyokoMethod : MonoBehaviour {
-    //upDownHantei
+    //縦横スワイプをを判定するメソッド
+
     //スワイプの距離を入れるために使う変数。
     Vector3 firstPos;
-    //上下判定で使う変数
-    int upDown = 0;
+    
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
@@ -15,20 +15,23 @@ public class t0043_ym_tateyokoMethod : MonoBehaviour {
             firstPos = Input.mousePosition;
         }
         if (Input.GetMouseButtonUp(0)) {
-            upDown = upDownHantei();
-            Debug.Log(upDown);
+           
+            Debug.Log(tateYokoHantei());
         }
     }
 
-    //マウス入力upは1、downは2,どちらでもないは0を返す。
-    int upDownHantei() {
+    //縦横スワイプをを判定するメソッド
+    int tateYokoHantei() {
         Vector3 diff = Input.mousePosition - firstPos;
-        if (diff.y > 0) {
-            //Debug.Log("up");
+        if (diff.x < 0) diff.x=diff.x * -1;
+        if (diff.y < 0) diff.y = diff.y* -1;
+
+        if (diff.x > diff.y) {
+            //横移動の方が大きければ１を返す
             return (1);
-        } else if (diff.y < 0) {
-            //Debug.Log("down");
+        } else {
+            //縦移動の方が大きいか同じならば２を返す
             return (2);
-        } else return (0);
+        }
     }
 }
