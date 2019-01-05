5c7b8e079415568bc51f9563194cb1a98ba64f53
georgeOoparts <urataffy@gmail.com>
2019-01-05 09:05:54 +0900

    ｋ１　a1デバックログ　表示

--- k0001_debugLog.cs
+++ k0001_debugLog.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 public class k0001_debugLog : MonoBehaviour {
 }
-/*k1　デバックログの使い方
+/*  k1　デバックログの使い方
     k1_1_a1 デバッグログ表示
     Debug.Log("デバックログはこうやるぜee");
     k1_1_a2 デバッグログ　変数　表示----ー----------------
