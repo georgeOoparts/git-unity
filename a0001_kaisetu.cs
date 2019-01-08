d70163f2cb9c08d2f618f7874dc4ad9f11c73e16
georgeOoparts <urataffy@gmail.com>
2019-01-08 11:43:52 +0900

    a1 解説　k3スクリーンポイント　追加　k9は　　　　　k3に合併

--- a0001_kaisetu.cs
+++ a0001_kaisetu.cs
@@ -4,66 +4,87 @@ using UnityEngine;
 
 public class a0001_kaisetu : MonoBehaviour {
 
-    //AC_a1:アンドロイド端末でステータスバーとナビゲーションバーを常に表示 
-    //オブジェにApplicationChrome.csをアタッチ。
-    //同じオブジェにスクリプトをアタッチ。
-    //スクリプトのスタートに下の文字列を書く。
-    //ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
+    
+}
+/*
+    AC_a1:アンドロイド端末でステータスバーとナビゲーションバーを常に表示 
+    オブジェにApplicationChrome.csをアタッチ。
+    同じオブジェにスクリプトをアタッチ。
+    スクリプトのスタートに下の文字列を書く。
+    ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
 
-    //AC_a2:ステータスバーだけを表示
-    //ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
-    //ApplicationChrome.navigationBarState=ApplicationChrome.State.Hidden;
+    AC_a2:ステータスバーだけを表示
+    ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
+    ApplicationChrome.navigationBarState=ApplicationChrome.State.Hidden;
 
-    //AC_a3:ナビゲーションバーだけを表示
-    //ApplicationChrome.navigationBarState=ApplicationChrome.State.Visible;
+    AC_a3:ナビゲーションバーだけを表示
+    ApplicationChrome.navigationBarState=ApplicationChrome.State.Visible;
 
-    //AC_b1:ステータスバー、ナビゲーションバーの状態を変える
-    //通常　ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
-    //画面に映る物の上にかぶせる
-    //ApplicationChrome.statusBarState=
-    //            ApplicationChrome.State.VisiblOverContent;
-    //透明になりながら画面に映るもののうえにかぶさる
-    //ApplicationChrome.statusBarState=
-    //            ApplicationChrome.State.TranslucentOverContent;
-    //隠す
-    //ApplicationChrome.statusBarState=ApplicationChrome.State.Hidden;
-    //------------------------------------------------------------------------------
+    AC_b1:ステータスバー、ナビゲーションバーの状態を変える
+    通常　ApplicationChrome.statusBarState=ApplicationChrome.State.Visible;
+    画面に映る物の上にかぶせる
+    ApplicationChrome.statusBarState=
+                ApplicationChrome.State.VisiblOverContent;
+    透明になりながら画面に映るもののうえにかぶさる
+    ApplicationChrome.statusBarState=
+                ApplicationChrome.State.TranslucentOverContent;
+    隠す
+    ApplicationChrome.statusBarState=ApplicationChrome.State.Hidden;
+    ------------------------------------------------------------------------------
 
-    //g1 最新のコミットメッセージを変更
-    //g1 ６下ファイルステータス＞８オブションのコミット＞最後のコミットを上書き
-    //g2 無駄に進んだ（枝別れでも良し）したブランチを戻す
-    //g2 無駄に進んだブランチを選択＞無駄に進んだブランチを右クリっク
-    //　＞このコミットを打ち消す＞枝分かれの根元を右クリック
-    // ＞このコミットまでリセット＞hard
-    //------------------------------------------------------------------------------
+    g1 最新のコミットメッセージを変更
+    g1 ６下ファイルステータス＞８オブションのコミット＞最後のコミットを上書き
+    g2 無駄に進んだ（枝別れでも良し）したブランチを戻す
+    g2 無駄に進んだブランチを選択＞無駄に進んだブランチを右クリっク
+    　＞このコミットを打ち消す＞枝分かれの根元を右クリック
+     ＞このコミットまでリセット＞hard
+    ------------------------------------------------------------------------------
 
-    //u1 呼び込まれるプログラムの順番　１上側＞edit>projectseting>scriptexecutionOrder  
-    //u1>数字が低いほうから読み込まれる。
-    //------------------------------------------------------------------------------
+    u1 呼び込まれるプログラムの順番　１上側＞edit>projectseting
+    >scriptexecutionOrder  
+    u1>数字が低いほうから読み込まれる。
+    ------------------------------------------------------------------------------
 
-    //k1::デバックログの使い方
-    //k1_1_a1:デバッグログ表示
-    //Debug.Log("デバックログはこうやるぜee");
-    //k1_1_a2:デバッグログ変数 表示----ー----------------
-    //int hensu = 9999;
-    //Debug.Log(hensu);
-    //k1_1_a3:デバッグログ変数＋　表示----ー----------------
-    //int hensu = 9999;
-    //Debug.Log("wowow"+hensu);
-    //----------------------------------------------------------
-    //k2_1_a1:どこかに書かれている。Textというクラスを扱うための変数を作成
-    //Text text;
-    // -----------------
-    // k2_1_a2:Textをこのオブジェクトで使うためのおまじない
-    // text = this.gameObject.GetComponent<Text>();
-    // -----------------
-    // k2_1_a3:text.text = "・・・ "でTEXTのないよう変更。
-    // text.text = "k2_1_TextContentChange";
-    // ------------------
-    // k2_1_a4:スクリーン座標のテキスト幅 text.preferredWidth
-    // k2_1_a5:スクリーン座標のテキスト高さ text.preferredHeight
+    k1::デバックログの使い方
+    k1_1_a1:デバッグログ表示
+    Debug.Log("デバックログはこうやるぜee");
+    k1_1_a2:デバッグログ変数 表示----ー----------------
+    int hensu = 9999;
+    Debug.Log(hensu);
+    k1_1_a3:デバッグログ変数＋　表示----ー----------------
+    int hensu = 9999;
+    Debug.Log("wowow"+hensu);
+    ----------------------------------------------------------
+    k2_1_a1:どこかに書かれている。Textというクラスを扱うための変数を作成
+    Text text;
+     -----------------
+     k2_1_a2:Textをこのオブジェクトで使うためのおまじない
+     text = this.gameObject.GetComponent<Text>();
+     -----------------
+     k2_1_a3:text.text = "・・・ "でTEXTのないよう変更。
+     text.text = "k2_1_TextContentChange";
+     ------------------
+     k2_1_a4:スクリーン座標のテキスト幅 text.preferredWidth
+     k2_1_a5:スクリーン座標のテキスト高さ text.preferredHeight
+    -------------------------------------------------------------
+    mouseposition回り
+    k0003_1_a1:Input.mousePosition.ToString() でマウスのスクリーンポイントを
+    string形式で代入
+    //具体例：：Debug.Log(Input.mousePosition.ToString());
+
+    k0003_1_b1:一瞬左クリックダウン入力されたか出力０１
+    Input.GetMouseButtonDown(0)
+    具体例：：if (Input.GetMouseButtonDown(0)){・・・}
+
+    k9_1_b2:一瞬左クリックアップ入力されたか出力０１Input.GetMouseButtonUp(0)
+    具体例：：if (Input.GetMouseButtonDown(0)){・・・}
+
+    k9_1_b3:左クリック押されてる間入力されたか出力０１
+    Input.GetMouseButtonDown(0)
+    具体例：：if (Input.GetMouseButton(0)){スライドするとか・・・}
+    
     //-------------------------------------------------------------
-    //k3_a:Input.mousePosition.ToString()でマウスのスクリーンポイント表示
+
     //k3_zz2_a:スクリーン座標＞ワールド座標
     //k4_a:どこかに書いてあるRectTransformの変数を作る
     //k4_aa:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
@@ -83,7 +104,7 @@ public class a0001_kaisetu : MonoBehaviour {
     //k8_a:子(孫、ひ孫　無限にＯＫ)になっているＯＢＪを得る。
     //k8_b:応用。親になっているＯＢＪを得る。
     //k8_b:oyaPanel = this.gameObject.transform.parent.gameObject;
-    //k9_a:左クリック入力　Input.GetMouseButtonDown(0)
+    
     //k10_kataHenkan
     //k10:float>int に小数点以下切り上げで変換。
     //k10:int kazu=(int)fKazu
@@ -113,10 +134,8 @@ public class a0001_kaisetu : MonoBehaviour {
     //k0014_b2 :プレハブを使う さらに内容変更
     //GameObject obj = Instantiate(page, placePosition, q);
     //obj.GetComponent<TextMesh>().text =i.ToString();
-
-
-
-
-
-    //＞
-}
+     
+     
+     
+     
+     */
--- k0003_screenMousePoint.cs
+++ k0003_screenMousePoint.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 //マウスのスクリーンポイントをdebugLogに表示
 public class k0003_screenMousePoint : MonoBehaviour {
-    // Update is called once per frame
-	void Update () {
-        //k3_a:Input.mousePosition.ToString()でマウスのスクリーンポイント表示
-        //Debug.Log(Input.mousePosition.ToString());
-    }
+    
 }
+/*  //mouseposition回り
+    k0003_1_a1:Input.mousePosition.ToString()でマウスのスクリーンポイントを
+    string形式で代入
+    //具体例：：Debug.Log(Input.mousePosition.ToString());
+    
+    k0003_1_b1:一瞬左クリックダウン入力されたか出力０１Input.GetMouseButtonDown(0)
+    //具体例：：if (Input.GetMouseButtonDown(0)){・・・}
+
+    //k9_1_b2:一瞬左クリックアップ入力されたか出力０１Input.GetMouseButtonUp(0)
+    //具体例：：if (Input.GetMouseButtonDown(0)){・・・}
+
+    //k9_1_b3:左クリック押されてる間入力されたか出力０１Input.GetMouseButtonDown(0)
+    //具体例：：if (Input.GetMouseButton(0)){スライドするとか・・・}
+     
+     */
