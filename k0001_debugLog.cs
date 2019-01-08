519a16f1a9c718d9db23113f8ce7d19545c1ca45
georgeOoparts <urataffy@gmail.com>
2019-01-09 08:46:48 +0900

    K1までOK

--- k0001_debugLog.cs
+++ k0001_debugLog.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 
 public class k0001_debugLog : MonoBehaviour {
 }
-/*  k1　デバックログの使い方
-    k1_1_a1 デバッグログ表示
+/*  //k1　デバックログの使い方
+    
+    //k1_1 デバッグログ表示
     Debug.Log("デバックログはこうやるぜee");
-    k1_1_a2 デバッグログ　変数　表示----ー----------------
+    
+    //k1_2 デバッグログ　変数　表示----ー----------------
+    ////?
     int hensu = 9999;
     Debug.Log(hensu);
-    k1_1_a3 デバッグログ　変数＋　表示----ー----------------
+    
+    //k1_3 デバッグログ　変数＋　表示----ー----------------
+    ////?
     int hensu = 9999;
     Debug.Log("wowow"+hensu);
 
