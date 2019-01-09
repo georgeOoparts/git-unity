ec3f43b1b8452f11282ae2c851cba49473c21e03
georgeOoparts <urataffy@gmail.com>
2019-01-09 09:36:51 +0900

    ｋ５までOK

--- k0005_atStatic.cs
+++ k0005_atStatic.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class k0005_atStatic : MonoBehaviour {
-    //k5_a_atStatic:静的変数を外のクラスから（クラス名）.(メソッド名)で呼び出す。
-    public static int c = 200;
-    //k5_b_atStatic:静的メソッドを外のクラスから（クラス名）.(メソッド名)で呼び出す。
-    public static int plus(int A, int B) {
-        return (A * B);
-    }
+   
 }
 /*
-    //k5_a_atStatic:静的変数を外のクラスから（クラス名）.(メソッド名)で呼び出す。
+    //k5 メソッド　変数　外部から呼び出す。
+    
+    //k5_1_atStatic:静的変数を外のクラスから（クラス名）.(メソッド名)で呼び出す。
     public static int c = 200;
-    //k5_b_atStatic:静的メソッドを外のクラスから（クラス名）.(メソッド名)で呼び出す。
+    
+    //k5_2_atStatic:静的メソッドを外のクラスから（クラス名）.(メソッド名)で呼び出す。
     public static int plus(int A, int B) {
         return (A * B);
     }
