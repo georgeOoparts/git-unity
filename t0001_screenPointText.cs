8e973c0c7c978daceed7a8474b58d902187d827e
georgeOoparts <urataffy@gmail.com>
2019-01-09 10:27:16 +0900

    ｔ１までOK

--- t0001_screenPointText.cs
+++ t0001_screenPointText.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;//UI使うときはこれなきゃ駄目
 
 //text ui にアタッチする。マウスのスクリーンポイントをUIのtextに表示
 public class t0001_screenPointText : MonoBehaviour {
-    //k2_a:どこかに書かれている。Textというクラスを扱うための変数を作成
+    //k2_1:どこかに書かれている。Textというクラスを扱うための変数を作成
     Text text;
-    // Use this for initialization
+
     void Start () {
-        //k2_aa:Textをこのオブジェクトで使うためのおまじない
+        //k2_1_1: Textをこのオブジェクトで使うためのおまじない
         text = this.gameObject.GetComponent<Text>();
     }
 	
-	// Update is called once per frame
 	void Update () {
-        //k2_aaa:text.text = "・・・ "でTEXTのないよう変更。
-        //k3_a:Input.mousePosition.ToString()でマウスのスクリーンポイント表示
+        //k2_1_1_1:text.text = "・・・ "でTEXTのないよう変更。
+        //k1_3 デバッグログ　変数＋　表示
+        //k0003_2:Input.mousePosition.ToString()でマウスのスクリーンポイントを
+        //string形式で代入
         text.text = "screen:: " + Input.mousePosition.ToString();
     }
 }
