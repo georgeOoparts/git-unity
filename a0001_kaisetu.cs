5929ec725bbca8f0fa7fbd86a33fe331ad140c2b
georgeOoparts <urataffy@gmail.com>
2019-01-09 18:05:44 +0900

    t1までOK　u1これはずっと残すシリーズに変換

--- a0001_kaisetu.cs
+++ a0001_kaisetu.cs
@@ -72,18 +72,20 @@ public class a0001_kaisetu : MonoBehaviour {
     ----------------------------------------------------------
     k2::Text回り
 
-    k2_1:どこかに書かれている。Textというクラスを扱うための変数を作成
+    //k2_1:どこかに書かれている。Textというクラスを扱うための変数を作成
     Text text;
     -----------------
-    k2_1_1:Textをこのオブジェクトで使うためのおまじない
+    //k2_1_1:Textをこのオブジェクトで使うためのおまじない
     text = this.gameObject.GetComponent<Text>();
     -----------------
-    k2_1_1_1:text.text = "・・・ "でTEXTのないよう変更。
+    //k2_1_1_1:text.text = "・・・ "でTEXTのないよう変更。
     text.text = "k2_1_TextContentChange";
     ------------------
-    k2_1_1_2:スクリーン座標のテキスト幅   text.preferredWidth
+    //k2_1_1_2:スクリーン座標のテキスト幅   
+    text.preferredWidth
     
-    k2_1_1_3:スクリーン座標のテキスト高さ text.preferredHeight
+    //k2_1_1_3:スクリーン座標のテキスト高さ 
+    text.preferredHeight
     ---------------------------------------------------------------------------------------
     mouseposition回り
    
