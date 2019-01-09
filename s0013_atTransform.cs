32dd49b5b715963decd82c176cb110a2f4d787a5
georgeOoparts <urataffy@gmail.com>
2019-01-09 17:52:15 +0900

    ｓシリーズは公理の最単純な具体例。本来なら、K公理１つにつき１つある。今回は省略。　これで解説１３までまじ完了？

---
+++ s0013_atTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s0013_atTransform : MonoBehaviour {
+    //k0013:３ｄオブジェtransfome回り
+
+    //k0013_1: 宣言 
+    Transform page;
+
+    void Start () {
+        //k0013_1_1;オブジェに当てはめる；
+        page = this.gameObject.GetComponent<Transform>();
+    }
+	
+	void Update () {
+        //k0013_1_1_1 オブジェ移動；
+        page.position = new Vector3((float)-2.8, -5, 0);
+    }
+}
