8ef7878efe107eab700ca58ffee95363ec95164b
georgeOoparts <urataffy@gmail.com>
2019-01-09 09:56:07 +0900

    ｋ６　ここまでOK

--- k0006_stopWatch.cs
+++ k0006_stopWatch.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-//k6_stopwatch:
+
 public class k0006_stopWatch : MonoBehaviour {
-    //k6_a:ストップウォッチ関数を使う時のおまじない。
+    
+}
+/*
+    //k6_stopwatch:
+    
+    //k6_1:ストップウォッチ関数を使う時のおまじない。
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
-    // 何秒たったかを変数elapseに入れる
-    //private float elapse;
-    //public float interval = 0.35f;
-    // Use this for initialization
-    void Start () {
-        //k6_aa:ストップウォッチスタート
-        stopwatch.Start();
-        //k6_ab:ストップウォッチの時間をリセット
-        stopwatch.Reset();
-        //k6_ac:何秒たったかを変数elapseに入れる
-        //elapse = (float)stopwatch.Elapsed.TotalSeconds;
-        //Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
-    }
-}
+    
+    //k6_1_1:何秒たったかを変数elapseに入れる-------
+    ////?
+    private float elapse;
+    
+    elapse = (float)stopwatch.Elapsed.TotalSeconds;
+    -------------------------------------------------
+
+    //k6_1_1_1:ストップウォッチスタート
+    stopwatch.Start();
+    
+    //k6_1_1_2:ストップウォッチの時間をリセット
+    stopwatch.Reset();
+    
+    具体例＞＞Debug.Log(elapse);//何秒たったかを表示させたいときはこれを使う
+    
+*/
