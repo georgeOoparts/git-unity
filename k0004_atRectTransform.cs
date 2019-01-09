0fd9e06be41fcdc2203160ecebdc33990d537583
georgeOoparts <urataffy@gmail.com>
2019-01-09 09:29:34 +0900

    ｋ４まで

--- k0004_atRectTransform.cs
+++ k0004_atRectTransform.cs
@@ -7,23 +7,23 @@ public class k0004_atRectTransform: MonoBehaviour {
     
 }
 /*
-    //k4_1_a1:どこかに書いてあるRectTransformの変数を作る
+    //k4_1:どこかに書いてあるRectTransformの変数を作る
     RectTransform rt;
 
-    //k4_1_a2:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
+    //k4_1_1:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
     rt = this.gameObject.GetComponent<RectTransform>();
 
-    //k4_a3_1:uiオブジェクトのスクリーン座標幅を得る
+    //k4_1_1_1:uiオブジェクトのスクリーン座標幅を得る
     rt.sizeDelta.x
-    //k4_a3_2:uiオブジェクトのスクリーン座標高さを得る
+    //k4_1_1_2:uiオブジェクトのスクリーン座標高さを得る
     rt.sizeDelta.y
 
-    //k4_a3_3:uiの幅、高さをスクリーン値で変形させる
+    //k4_1_1_3:uiの幅、高さをスクリーン値で変形させる
     rt.sizeDelta = new Vector2(100,100);
 
-    //k4_a3_4:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
+    //k4_1_1_4:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
     rt.anchoredPosition = new Vector2(190, -145);
-    //k4_a3_5:uiをワールド値で移動
+    //k4_1_1_5:uiをワールド値で移動
     rt.position = new Vector2(0,0);
      
      */
